81ac8b3f87d2de85cdff852536fb7b177a3e7177
addakridler <addakridler@gmail.com>
2021-08-18 11:23:44 -0400

    updated mongo uri

--- app/server/messageModel.js
+++ app/server/messageModel.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb://localhost:27017';
+const MONGO_URI = 'mongodb+srv://new_user_1:XfbblulTvlrqrVCZ@cluster0.h6v1s.mongodb.net/Periscope?retryWrites=true&w=majority';
 mongoose.connect(MONGO_URI, {
   // options for the connect method to parse the URI
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // sets the name of the DB that our collections are part of
-  dbName: 'periscope',
+  dbName: 'Periscope',
 })
   .then(() => {
     console.log('Connected to Mongo DB.');
