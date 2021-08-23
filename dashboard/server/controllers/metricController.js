b0fb6a2da58ea3f52dc2ae7ba5c45d694314b03f
Shawn Convery <shawn@Shawns-MacBook-Pro-3.local>
2021-08-23 12:29:07 -0400

    cpu usage controller

--- dashboard/server/controllers/metricController.js
+++ dashboard/server/controllers/metricController.js
@@ -2,10 +2,12 @@ const fetch = require('node-fetch');
 
 const metricController = {};
 
+const currentDate = Math.floor(Date.now() / 1000);
+const startDate = currentDate - 604800;
+console.log('date: ', currentDate);
+
 metricController.getMetrics = async (req, res, next) => {
-  const currentDate = new Date().toISOString();
-  console.log('date: ', currentDate);
-  const query = `http://localhost:9090/api/v1/query?query=sum(rate(container_cpu_usage_seconds_total{image!=""}[2m]))by(pod)&start=${currentDate}&end=${currentDate}&step=1m`;
+  const query = `http://localhost:9090/api/v1/query?query=sum(rate(container_cpu_usage_seconds_total{image!=""}[2m]))by(pod)&start=${startDate}&end=${currentDate}&step=1m`;
 
   try {
     console.log('in metricsController.getMetric');
@@ -18,7 +20,18 @@ metricController.getMetrics = async (req, res, next) => {
   }
 };
 
+metricController.getPodCPU = async (req, res, next) => {
+  const query = `http://localhost:9090/api/v1/query_range?query=sum(rate(container_cpu_usage_seconds_total{image!=%22%22}[5m]))by(node)&start=${startDate}&end=${currentDate}&step=5m`;
 
-
+  try {
+    console.log('in metricsController.getMetric');
+    const response = await fetch(query);
+    res.locals.podCPU = await response.json();
+    // console.log('locals: ', res.locals.query);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+};
 
 module.exports = metricController;
