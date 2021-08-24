4a848b5cd0ab77ec5062ee1fd59e6901f44e4ebb
ronke11 <rsoyekunle@gmail.com>
2021-08-24 13:23:24 -0400

    Updates disk usage

--- dashboard/client/components/CPU.jsx
+++ dashboard/client/components/CPU.jsx
@@ -27,24 +27,24 @@ const CPU = (props) => {
     <div>
       <h2>CPU Usage</h2>
       <ResponsiveContainer>
-      <LineChart
-          width={500}
-          height={300}
-          data={resultArr}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis scaleToFit={true} tick={{fontSize: 10}} dataKey="time" ticks={[20, 40, 60, 80, 100]}/>
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          {lines || null}
-        </LineChart>
+        <LineChart
+            width={500}
+            height={300}
+            data={resultArr}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis scaleToFit={true} tick={{fontSize: 10}} dataKey="time" ticks={[20, 40, 60, 80, 100]}/>
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            {lines || null}
+          </LineChart>
         </ResponsiveContainer>
       </div>
 
@@ -52,49 +52,3 @@ const CPU = (props) => {
 };
 
 export default CPU;
-
-
-  // const data01 = [
-  //   {
-  //     name: 'Page A',
-  //     uv: 4000,
-  //     pv: 2400,
-  //     amt: 2400,
-  //   },
-  //   {
-  //     name: 'Page B',
-  //     uv: 3000,
-  //     pv: 1398,
-  //     amt: 2210,
-  //   },
-  //   {
-  //     name: 'Page C',
-  //     uv: 2000,
-  //     pv: 9800,
-  //     amt: 2290,
-  //   },
-  //   {
-  //     name: 'Page D',
-  //     uv: 2780,
-  //     pv: 3908,
-  //     amt: 2000,
-  //   },
-  //   {
-  //     name: 'Page E',
-  //     uv: 1890,
-  //     pv: 4800,
-  //     amt: 2181,
-  //   },
-  //   {
-  //     name: 'Page F',
-  //     uv: 2390,
-  //     pv: 3800,
-  //     amt: 2500,
-  //   },
-  //   {
-  //     name: 'Page G',
-  //     uv: 3490,
-  //     pv: 4300,
-  //     amt: 2100,
-  //   },
-  // ];
--- dashboard/client/components/DiskUsage.jsx
+++ dashboard/client/components/DiskUsage.jsx
@@ -27,22 +27,18 @@ const DiskUsage = (props) => {
 
   // loop through freediskspace and get the times
   if (total && free) {
+
     // loops through totalDiskSpace query and pushes the name of node and total diskspace of node into an object
     for (let i = 0; i < total.length; i++) {
       // push each node #: diskSpace
       nodes[`node${i + 1}`] = total[i].value[1];
     }
-    console.log(nodes)
 
-    // console.log(free);
 
     // loops through FreeDiskSpace and sends time and value @ time to new object
     for (let i = 0; i < free.length; i++) {
-      // each free[index].metric.instance ==> name of the node
       const nodeNum = `node${i + 1}`;
       const values = free[i].values;
-      // free[index].values is an array
-      // loop through the array and each index in that array [time, value]
 
       // grab all the times from the first index of the array
       if (i === 0) {
@@ -72,56 +68,10 @@ const DiskUsage = (props) => {
   const lines = [];
   for (let i = 0; i < numNodes.length; i++) {
     lines.push(
-      <Line type='monotone' dataKey={`node${i+1}`} key={i} stroke={lineColors[i]} />
+      <Line type='monotone' dataKey={numNodes[i]} key={i} stroke={lineColors[i]} />
     );
   }
 
-  // console.log(data);
-
-  // const dataNm = [
-  //   {
-  //     name: 'Page A',
-  //     uv: 4000,
-  //     pv: 2400,
-  //     amt: 2400,
-  //   },
-  //   {
-  //     name: 'Page B',
-  //     uv: 3000,
-  //     pv: 1398,
-  //     amt: 2210,
-  //   },
-  //   {
-  //     name: 'Page C',
-  //     uv: 2000,
-  //     pv: 9800,
-  //     amt: 2290,
-  //   },
-  //   {
-  //     name: 'Page D',
-  //     uv: 2780,
-  //     pv: 3908,
-  //     amt: 2000,
-  //   },
-  //   {
-  //     name: 'Page E',
-  //     uv: 1890,
-  //     pv: 4800,
-  //     amt: 2181,
-  //   },
-  //   {
-  //     name: 'Page F',
-  //     uv: 2390,
-  //     pv: 3800,
-  //     amt: 2500,
-  //   },
-  //   {
-  //     name: 'Page G',
-  //     uv: 3490,
-  //     pv: 4300,
-  //     amt: 2100,
-  //   },
-  // ];
 
   return (
     <div>
