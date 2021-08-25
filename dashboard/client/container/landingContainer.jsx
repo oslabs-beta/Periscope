4d90cf9924f361e505dfe8c1eb4a834bc00b2e32
Shawn Convery <shawn@shawns-mbp-3.myfiosgateway.com>
2021-08-25 16:09:56 -0400

    new landing

--- dashboard/client/container/landingContainer.jsx
+++ dashboard/client/container/landingContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../assets/PeriscopeLogoTransparent.png';
+import Logo from '../assets/periscopeLogoTransparent.png';
 
 const LandingContainer = () => {
   useEffect(() => {
