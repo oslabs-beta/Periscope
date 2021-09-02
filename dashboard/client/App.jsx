1710c1a2cf94f0f4bd7c969dd08c301196c89672
Shawn Convery <shawn@shawns-mbp-3.myfiosgateway.com>
2021-09-02 14:00:27 -0400

    query working

--- dashboard/client/App.jsx
+++ dashboard/client/App.jsx
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MainContainer from './container/mainContainer.jsx';
 import LandingContainer from './container/landingContainer.jsx';
 import mainContainerGraphQL from './container/mainContainerGraphQL.js';
 
 const App = () => {
+  // useEffect with prometheus port forwarding 
+    useEffect(() => {
+    fetch('/prometheus').then((data) => {
+      console.log('connected to prometheus', data);
+    });
+  }, []);
+
   return (
     <Router>
       <Route exact path="/" component={LandingContainer} />
--- dashboard/client/container/mainContainerGraphQL.js
+++ dashboard/client/container/mainContainerGraphQL.js
@@ -16,93 +16,96 @@ const mainContainerGraphQL = () => {
   const [called, setCalled] = useState(false);
 
   const sixHours = 21600;
-  // const endTime = Math.floor(Date.now() / 1000);
-  const startTime = "1630286575";
-  const endTime = "1630358575";
-  // const startTime = endTime - sixHours;
-  const step = "5m";
-  
+  const endTimeNum = Math.floor(Date.now() / 1000);
+  // const startTime = '1630286575';
+  // const endTime = new Date.now() / 1000;
+  const startTimeNum = endTimeNum - sixHours;
+  const startTime = startTimeNum.toString();
+  const endTime = endTimeNum.toString();
 
-  useEffect( () => {
+  const step = '5m';
+
+  const query = `{
+    getFreeDiskSpace(startTime: "${startTime}", endTime: "${endTime}", step: "${step}") {
+      data {
+        result {
+          metric {
+            instance
+          }
+          values
+        }
+      }
+    }
+    getNodeCpu(startTime: "${startTime}", endTime: "${endTime}", step: "${step}") {
+      data {
+        result {
+          metric {
+            instance
+          }
+          values
+        }
+      }
+    }
+    getClusterInfo {
+      data {
+        result {
+          metric {
+            internal_ip
+            node
+          }
+          value
+        }
+      }
+    }
+    getNodeMemory {
+      data {
+        result {
+          metric {
+            instance
+          }
+          value
+        }
+      }
+    }
+    getTotalDiskSpace {
+      data {
+        result {
+          metric {
+            instance
+          }
+          value
+        }
+      }
+    }
+  }`;
+
+  useEffect(() => {
+    console.log('times: ', startTime, endTime);
     fetch('/graphql', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        query: `{
-          getFreeDiskSpace(startTime: "1630524326" , endTime: "1630596330", step: "5m") {
-            data {
-              result {
-                metric {
-                  instance
-                }
-                values
-              }
-            }
-          }
-          getNodeCpu(startTime: "1630524326", endTime: "1630596330", step: "5m") {
-            data {
-              result {
-                metric {
-                  instance
-                }
-                values
-              }
-            }
-          }
-          getClusterInfo {
-            data {
-              result {
-                metric {
-                  internal_ip
-                  node
-                }
-                value
-              }
-            }
-          }
-          getNodeMemory {
-            data {
-              result {
-                metric {
-                  instance
-                }
-                value
-              }
-            }
-          }
-          getTotalDiskSpace {
-            data {
-              result {
-                metric {
-                  instance
-                }
-                value
-              }
-            }
-          }
-        }`
-      })
-    }).then((res)=> res.json()).then(res => {
-      const data = res.data
-      console.log(data)
-      console.log('data.getNodeCpi', data.getNodeCpu);
-      setCPU(data.getNodeCpu);
-      setTotalDisk(data.getTotalDiskSpace);
-      setFreeDisk(data.getFreeDiskSpace);
-      setNodeMemory(data.getNodeMemory);
-      setClusterInfo(data.getClusterInfo);
-      setIsLoading(false);
-
-
+        query,
+      }),
     })
-  
-   
+      .then((res) => res.json())
+      .then((res) => {
+        const data = res.data;
+        console.log(data);
+        console.log('data.getNodeCpi', data.getNodeCpu);
+        setCPU(data.getNodeCpu);
+        setTotalDisk(data.getTotalDiskSpace);
+        setFreeDisk(data.getFreeDiskSpace);
+        setNodeMemory(data.getNodeMemory);
+        setClusterInfo(data.getClusterInfo);
+        setIsLoading(false);
+      });
   }, []);
 
   if (!isLoading && !called) {
-    console.log('clusterinfo', clusterInfo)
+    console.log('clusterinfo', clusterInfo);
     const result = [];
     for (let i = 1; i <= clusterInfo.data.result.length; i++) {
       // create nodes 1 through x based on internal Ip addresses
@@ -112,9 +115,9 @@ const mainContainerGraphQL = () => {
     setCalled(true);
   }
 
-   return ((isLoading) ? 
+  return isLoading ? (
     <img id='loading' src={loading} />
-    : 
+  ) : (
     <div className='main-container'>
       <div id='CPU' className='components'>
         <CPU cpu={cpu} nodeNums={nodeNums} />
