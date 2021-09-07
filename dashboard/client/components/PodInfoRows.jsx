cdde2e98857a130693b5f23c1f28b3b2f821cb8a
Shawn Convery <shawn@shawns-mbp-3.myfiosgateway.com>
2021-09-07 12:27:36 -0400

    table work

--- dashboard/client/components/PodInfoRows.jsx
+++ dashboard/client/components/PodInfoRows.jsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import DropdownButton from 'react-bootstrap/esm/DropdownButton';
 import DropdownItem from 'react-bootstrap/esm/DropdownItem';
+import PodInfoTableSetup from './PodInfoTableSetup.jsx';
 
-const PodInfoRows = ({ clickedArray, setClickedArray, podNums, setStep, setTimeWindow }) => {
-
+const PodInfoRows = ({
+  clickedArray,
+  setClickedArray,
+  podNums,
+  setStep,
+  setTimeWindow,
+}) => {
   // on click function for pod names list to set clickedArray in podContainer
   const newClick = (arg) => {
     let found = false;
@@ -22,24 +28,24 @@ const PodInfoRows = ({ clickedArray, setClickedArray, podNums, setStep, setTimeW
     }
   };
 
-  // create list of pod names 
-  const podNames = Object.entries(podNums);
-  const podList = [];
-  for (let i = 0; i < podNames.length; i++) {
-    let pod = podNames[i][1];
-    podList.push(
-      <li
-        style={{ "list-style-type": "none" }}
-        onClick={() => {
-          newClick(pod.podName);
-        }}
-        key={i}>
-        {pod.name} {pod.podName}
-      </li>
-    );
-  }
+  // create list of pod names
+  // const podNames = Object.entries(podNums);
+  // const podList = [];
+  // for (let i = 0; i < podNames.length; i++) {
+  //   let pod = podNames[i][1];
+  //   podList.push(
+  //     <li
+  //       style={{ "list-style-type": "none" }}
+  //       onClick={() => {
+  //         newClick(pod.podName);
+  //       }}
+  //       key={i}>
+  //       {pod.name} {pod.podName}
+  //     </li>
+  //   );
+  // }
 
-   /* -----------------------------------------------------------------------------------------
+  /* -----------------------------------------------------------------------------------------
   functionality to change time and step range - 
   to disable functionality: 
   comment out this section and div w/ id of 'dropdowns' in podInfoRows render
@@ -54,33 +60,60 @@ const PodInfoRows = ({ clickedArray, setClickedArray, podNums, setStep, setTimeW
 
   const times = [oneHour, sixHours, twelveHours, oneDay, threeDays];
   const timeButtons = [];
-  times.forEach((time, i) => { //create dropwdown items to select time range
-    timeButtons.push(<DropdownItem menuVariant="dark" key={i}> <div onClick={() => { console.log('clicked'); setTimeWindow(time); setClickedArray([]) }} style={{ padding: '5px'}}>{time}</div></DropdownItem>);
-  })
+  times.forEach((time, i) => {
+    //create dropwdown items to select time range
+    timeButtons.push(
+      <DropdownItem menuVariant='dark' key={i}>
+        {' '}
+        <div
+          onClick={() => {
+            console.log('clicked');
+            setTimeWindow(time);
+            setClickedArray([]);
+          }}
+          style={{ padding: '5px' }}>
+          {time}
+        </div>
+      </DropdownItem>
+    );
+  });
 
   const steps = ['1m', '5m', '30m', '1hr']; // step range variables in array
   const stepButtons = [];
-  steps.forEach((step, i) => { // create dropdown items to select step range
-    stepButtons.push(<DropdownItem menuVariant="dark" key={i}> <div onClick={() => { console.log('clicked'); setStep(step); setClickedArray([]) }} style={{ padding: '5px'}}>{step}</div></DropdownItem>);
-  })
+  steps.forEach((step, i) => {
+    // create dropdown items to select step range
+    stepButtons.push(
+      <DropdownItem menuVariant='dark' key={i}>
+        {' '}
+        <div
+          onClick={() => {
+            console.log('clicked');
+            setStep(step);
+            setClickedArray([]);
+          }}
+          style={{ padding: '5px' }}>
+          {step}
+        </div>
+      </DropdownItem>
+    );
+  });
   // ------------------------------------------------------------------------------------------
-
 
   return (
     <div>
       <div id='dropdowns'>
-        <DropdownButton class="dropdown-button" title="Time Range">
+        <DropdownButton class='dropdown-button' title='Time Range'>
           {timeButtons}
         </DropdownButton>
-        <DropdownButton class="dropdown-button" title="Step">
+        <DropdownButton class='dropdown-button' title='Step'>
           {stepButtons}
         </DropdownButton>
       </div>
-      {' '}
-      <ul>{podList}</ul>{' '}
+      {/* {' '}
+      <ul>{podList}</ul>{' '} */}
+      <PodInfoTableSetup podNums={podNums} newClick={newClick} />
     </div>
   );
 };
 
 export default PodInfoRows;
-
